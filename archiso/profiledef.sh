3a8f5f5b7dae75b7b946037f6d6f54de00ba1418
Erik Dubois <erik.dubois@gmail.com>
2026-05-03 06:42:27 +0200

    update

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="kiro"
-iso_label="kiro-v26.05.02.01"
+iso_label="kiro-v26.05.03.01"
 iso_publisher="kiro"
 iso_application="Kiro Live/Rescue CD"
-iso_version="v26.05.02.01"
+iso_version="v26.05.03.01"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
--- build-scripts/build-the-iso.sh
+++ build-scripts/build-the-iso.sh
@@ -125,7 +125,7 @@ echo
 
 	desktop="xfce4/edu-chadwm/ohmychadwm"
 
-	kiroVersion='v26.05.02.01'
+	kiroVersion='v26.05.03.01'
 
 	isoLabel='kiro-'$kiroVersion'-x86_64.iso'
 
